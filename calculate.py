7ab32bb5fcd201731d8dfa83570983beb251e135
Yegua Celestial <yeguacelestial@gmail.com>
2020-03-25 16:11:18 -0600

    Created Coronavirus class, fetch_data and getCountry methods

--- calculate.py
+++ calculate.py
@@ -21,3 +21,27 @@
 #I(miercoles) = I(martes) * F-actual
 #I(miercoles) = 367 * 1.1613
 #I(miercoles) = 426
+
+from selenium import webdriver
+
+class Coronavirus():
+
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get('https://www.worldometers.info/coronavirus/')
+    
+    def fetch_data(self):
+        today_table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]')
+        yesterday_table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_yesterday"]')
+        
+        country_element = today_table.find_element_by_xpath("//td[contains(text(), 'Mexico')]")
+        row = country_element.find_element_by_xpath("./..")
+        data = row.text.split(" ")
+        return data
+    
+    def getCountry(self):
+        data = self.fetch_data()
+        country = data[0]
+        return print(country)
+        
+
