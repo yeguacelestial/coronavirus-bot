af4ee562a32068678c0c7868eceb3c23ff82b4a5
Yegua Celestial <yeguacelestial@gmail.com>
2020-04-04 23:34:57 -0600

    Created time int values

--- values.py
+++ values.py
@@ -7,3 +7,8 @@ consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret_key = os.getenv('CONSUMER_SECRET_KEY')
 access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+minute = 60
+hour = minute * 60
+day = hour * 24
+week = day * 7
